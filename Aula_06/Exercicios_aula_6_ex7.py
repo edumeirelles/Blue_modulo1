54abedeef71c031cf70d6ea5db51dc23e88ac021
Eduardo <edumeirelles@yahoo.com.br>
2021-05-12 17:57:55 -0300

    Correção exercicio 7

--- Aula_06/Exercicios_aula_6_ex7.py
+++ Aula_06/Exercicios_aula_6_ex7.py
@@ -1,11 +1,11 @@
 # 7. Escreva uma função que recebe dois parâmetros e imprime o menor dos dois. Se eles forem iguais, imprima que eles são iguais.
 
-def maior(x, y):
-    if x > y:
+def menor(x, y):
+    if x < y:
         return x
-    elif x < y:
+    elif x > y:
         return y
     else:
         return 'Os números são iguais'
 
-print('Maior número =', maior(float(input('Digite o primeiro número: ').replace(',','.')), float(input('Digite o segundo número: ').replace(',','.'))))
+print('Menor número =', menor(float(input('Digite o primeiro número: ').replace(',','.')), float(input('Digite o segundo número: ').replace(',','.'))))
