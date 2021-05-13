f30a0cfcf9b8ae7f2686fdc8932a9a163536e9bf
Eduardo <edumeirelles@yahoo.com.br>
2021-05-12 23:04:10 -0300

    Aula 8

---
+++ Aula_08/Aula08.py
@@ -0,0 +1,12 @@
+def testa_idade(idade=18):
+    #idade = int(input('Digite a idade: '))
+
+    if idade >= 18:
+        print('Maior de idade')
+    else:
+        print('Menor de idade')
+
+testa_idade(30)
+
+lista1 = [1, 2, 3]
+print(lista1[0])
