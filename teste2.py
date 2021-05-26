89fe3c5a2cfd63c3456940f881a5c144482d9c2f
Eduardo <edumeirelles@yahoo.com.br>
2021-05-26 18:38:35 -0300

    Projeto Jogo

--- teste2.py
+++ teste2.py
@@ -1,6 +1,8 @@
+from time import sleep
+
 class Personagem:
     def __init__(self):
-        self.habilidade = 50
+        self.habilidade = 0
         self.cansaco = 0
         self.fama = 0
         self.dinheiro = 0
@@ -15,33 +17,77 @@ class Publico:
  
 if(__name__ == "__main__"):
     
+    print("---")
+    print('Bem vindo à banda Turma 2B!!!')
+    print("---")
+    sleep(1)
+
     personagem = Personagem()
     publico = Publico()   
+    musicas_tocadas = 0
+    subiu_no_palco = False
+    
     while True:
-        print("---")
-        print('Bem vindo à banda Turma 2B!!!')
+                
         print(personagem)
+        sleep(1)
         print("")
         print("Ações:")
-        print('1 - Cumprimentar o público')
-        print('2 - Tocar uma música')
-        print('3 - Abandonar Palco')
+        print('1 - Subir no palco')
+        print('2 - Cumprimentar o público')
+        print('3 - Tocar uma música')
+        print('4 - Fazer uma pausa')
+        print('5 - Abandonar Palco')
         print('0 - Sair')
         opcao = input("Escolha sua ação:")
+        
         if opcao == '1':
             personagem.fama += 5
             publico.publico += 100
         
-        
-        
-        if opcao == '3':
-            print('Você Abandonou o palco!')
-            if personagem.fama >= 10:
-                personagem.fama -= 10
+        elif opcao == '2':
+            personagem.fama += 10
+            publico.publico += 100
+                       
+        elif opcao == '3':
+            if personagem.cansaco >= 60:
+                print('Você está muito cansado!!! Melhor fazer uma pausa!!!')
             else:
-                personagem.fama = 0
+                if personagem.habilidade >= 75:
+                    personagem.habilidade += 5
+                    personagem.cansaco += 5
 
-        print(f'Sua habilidade é, {personagem.habilidade}, Seu cansaço é, {personagem.cansaco}, Sua fama é {personagem.fama}, e sua conta bancária é {personagem.dinheiro}.')
+                elif 75 > personagem.habilidade >= 50:
+                    personagem.habilidade += 5
+                    personagem.cansaco += 10
+
+                elif 50 > personagem.habilidade >= 25:
+                    personagem.habilidade += 10
+                    personagem.cansaco += 15
+
+                else:
+                    personagem.habilidade += 10
+                    personagem.cansaco += 20
+            musicas_tocadas += 1
+            personagem.dinheiro += musicas_tocadas*100
+        
+        elif opcao == '4':
+            personagem.cansaco -= 40
+            publico.publico *= 0.85
+
+
+        elif opcao == '5':
+            print('Você Abandonou o palco!')
+            personagem.dinheiro -= 1000
+            personagem.fama *= 0.5
+            publico.publico *= 0.25
+               
+            
+
+        elif opcao == "0":
+            break
+
+       # print(f'Sua habilidade é, {personagem.habilidade}, Seu cansaço é, {personagem.cansaco}, Sua fama é {personagem.fama}, e sua conta bancária é {personagem.dinheiro}.')
 
 
 
